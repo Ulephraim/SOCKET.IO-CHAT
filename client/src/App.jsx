1385cde1037ffdd98e9920643880e8e1ba17e0c8
Ulephraim <iphraimunlimited@gmail.com>
2025-03-04 21:01:15 +0100

    Implement socket.io

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,38 @@
 /** @format */
 
+import { useState } from 'react';
 import './App.css';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3001');
 
 function App() {
-  return <>Hello Socket.IO</>;
+  const [username, setUsername] = useState();
+  const [room, setRoom] = useState();
+
+  const joinRoom = () => {
+    if (username != '' && room != '') {
+      socket.emit('join_room', room);
+    }
+  };
+
+  return (
+    <>
+      Hello Socket.IO
+      <h3>Join Chat</h3>
+      <input
+        type="text"
+        placeholder="John..."
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Room ID..."
+        onChange={(e) => setRoom(e.target.value)}
+      />
+      <button onClick={joinRoom}>Join a Room</button>
+    </>
+  );
 }
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -8,14 +8,27 @@ const cors = require('cors');
 const { Server } = require('socket.io');
 app.use(cors());
 
+const server = http.createServer(app);
+
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST'],
   },
 });
 
-const server = http.createServer(app);
+io.on('connection', (socket) => {
+  console.log(`User connected  : ${socket.id}`);
+
+  socket.on('join_room', (data) => {
+    socket.join(data);
+    console.log(`User with ID : ${socket.id} joined room ${data}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User Disconnected', socket.id);
+  });
+});
 
 server.listen(3001, () => {
   console.log('server is running');
