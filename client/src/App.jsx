4dcba19167fcb23aaa0f70f4c05253f2edf6cec2
Ulephraim <iphraimunlimited@gmail.com>
2025-03-04 22:32:34 +0100

    setup Room messaging

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,34 +3,42 @@
 import { useState } from 'react';
 import './App.css';
 import io from 'socket.io-client';
+import Chat from './Components/Chat';
 
 const socket = io.connect('http://localhost:3001');
 
 function App() {
   const [username, setUsername] = useState();
   const [room, setRoom] = useState();
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
     if (username != '' && room != '') {
       socket.emit('join_room', room);
+      setShowChat(true);
     }
   };
 
   return (
     <>
-      Hello Socket.IO
-      <h3>Join Chat</h3>
-      <input
-        type="text"
-        placeholder="John..."
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Room ID..."
-        onChange={(e) => setRoom(e.target.value)}
-      />
-      <button onClick={joinRoom}>Join a Room</button>
+      {!showChat ? (
+        <div>
+          <h3>Join Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(e) => setRoom(e.target.value)}
+          />
+          <button onClick={joinRoom}>Join a Room</button>{' '}
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </>
   );
 }
